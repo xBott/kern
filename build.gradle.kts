75fc20f8f8827bfb06afb08e0132f5cf3c01d623
_xBott <roma.plakhotniuk07@gmail.com>
2026-03-07 01:18:01 +0100

    configured maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import io.github.klahap.dotenv.DotEnvBuilder
+
 plugins {
     java
+    id("io.github.klahap.dotenv") version "1.1.3"
+    id("maven-publish")
 }
 
 allprojects {
@@ -13,7 +17,13 @@ allprojects {
 
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "io.github.klahap.dotenv")
     apply(plugin = "maven-publish")
+
+    val envVars = DotEnvBuilder.dotEnv {
+        addFile("$rootDir/.env")
+    }
+    extra["envVars"] = envVars
 
     dependencies {
 
@@ -25,6 +35,30 @@ subprojects {
 
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = project.name
+                url = uri("https://repo.nimbra.net/releases")
+                credentials {
+                    username = envVars["REPO_USERNAME"].toString()
+                    password = envVars["REPO_PASSWORD"].toString()
+                }
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "me.bottdev"
+                artifactId = project.name
+                version = "${project.version}"
+                from(components["java"])
+            }
+        }
     }
 
     tasks.test {
