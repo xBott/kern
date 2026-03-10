4533b1d76d04a9fa2d33525ef869b59c1608b9d0
_xBott <roma.plakhotniuk07@gmail.com>
2026-03-10 19:09:56 +0100

    added some default methods for getting edges that connect some nodes

--- kern-struct/src/main/java/me/bottdev/kern/struct/graph/Graph.java
+++ kern-struct/src/main/java/me/bottdev/kern/struct/graph/Graph.java
@@ -2,7 +2,9 @@ package me.bottdev.kern.struct.graph;
 
 import me.bottdev.kern.struct.property.PropertyHolder;
 
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public interface Graph<N, E extends EndpointPair<N>> extends PropertyHolder {
 
@@ -23,6 +25,15 @@ public interface Graph<N, E extends EndpointPair<N>> extends PropertyHolder {
 
     boolean hasNode(N node);
     boolean hasEdgeConnecting(N nodeU, N nodeV);
+    default Set<E> edgesConnecting(N nodeU, N nodeV) {
+        return outEdges(nodeU).stream()
+                .filter(edge -> edge.hasEndpoint(nodeV))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    default Optional<E> edgeConnecting(N nodeU, N nodeV) {
+        return edgesConnecting(nodeU, nodeV).stream().findFirst();
+    }
 
 
     default int nodeCount() { return nodes().size(); }
