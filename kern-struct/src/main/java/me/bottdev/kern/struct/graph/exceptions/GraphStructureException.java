8e5dd17dac8af64c2be8f42d22162db4b0790b54
_xBott <roma.plakhotniuk07@gmail.com>
2026-03-10 19:10:49 +0100

    exception for graphs

---
+++ kern-struct/src/main/java/me/bottdev/kern/struct/graph/exceptions/GraphStructureException.java
@@ -0,0 +1,8 @@
+package me.bottdev.kern.struct.graph.exceptions;
+
+public class GraphStructureException extends RuntimeException {
+    public GraphStructureException(String message) {
+        super(message);
+    }
+
+}
