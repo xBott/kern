4382de198781d65ed18c8fad9e4f0ac1fa7f213e
_xBott <roma.plakhotniuk07@gmail.com>
2026-03-11 09:26:06 +0100

    deleted dijkstra exception

--- kern-struct/src/main/java/me/bottdev/kern/struct/algorithms/shortestpath/DijkstraException.java
+++
@@ -1,7 +0,0 @@
-package me.bottdev.kern.struct.algorithms.shortestpath;
-
-public class DijkstraException extends RuntimeException {
-    public DijkstraException(String message) {
-        super(message);
-    }
-}
