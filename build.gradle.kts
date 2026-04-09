ca21cba941f2e4b95de6c24bd287d6c3f6e4cc46
_xBott <roma.plakhotniuk07@gmail.com>
2026-04-09 19:35:38 +0200

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val rootEnvVars = DotEnvBuilder.dotEnv {
 
 allprojects {
     project.group = "me.bottdev"
-    project.version = "0.0.8-SNAPSHOT"
+    project.version = "0.0.9-SNAPSHOT"
 
     repositories {
         mavenCentral()
