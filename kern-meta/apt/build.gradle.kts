0302c942810b93833b95c0e01fb4ee6647223d79
_xBott <roma.plakhotniuk07@gmail.com>
2026-07-14 16:40:09 +0300

    chore: added Java Annotation Processor Module

---
+++ kern-meta/apt/build.gradle.kts
@@ -0,0 +1,18 @@
+dependencies {
+    implementation(project(":kern-commons"))
+    implementation(project(":kern-meta:core"))
+
+    implementation(libs.guava)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit.jupiter)
+
+    testImplementation(libs.assertj.core)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+}
