309b42e049d484b1e42471305e7fa54ff25f9537
_xBott <roma.plakhotniuk07@gmail.com>
2026-07-14 12:50:12 +0300

    docs: added JavaDoc

--- kern-struct/src/main/java/me/bottdev/kern/struct/PathStructure.java
+++ kern-struct/src/main/java/me/bottdev/kern/struct/PathStructure.java
@@ -1,5 +1,7 @@
 package me.bottdev.kern.struct;
 
+/// An interface for data-structures that are used for path algorithms
+/// Composes 3 interfaces: [ElementProvider] [NeighborProvider] [WeightProvider]
 public interface PathStructure<N> extends ElementProvider<N>, NeighborProvider<N>, WeightProvider<N> {
 
 
