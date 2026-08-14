9adfab8af0ddc6d982586cc59ebc7e23bac071c9
_xBott <roma.plakhotniuk07@gmail.com>
2026-08-15 01:45:14 +0200

    removed

--- kern-dependency/src/main/java/me/bottdev/kern/dependency/versioned/VersionConflict.java
+++
@@ -1,8 +0,0 @@
-package me.bottdev.kern.dependency.versioned;
-
-import java.util.List;
-
-public record VersionConflict<K>(
-        K dependencyKey,
-        List<VersionConflictEntry<K>> entries
-) {}
