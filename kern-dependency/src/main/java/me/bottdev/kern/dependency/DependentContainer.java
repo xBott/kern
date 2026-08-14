e3cb65be7b815168d195d8fad8397e8e535f1bb2
_xBott <roma.plakhotniuk07@gmail.com>
2026-08-15 01:46:19 +0200

    feat: added more methods to the interface and made it more useful

--- kern-dependency/src/main/java/me/bottdev/kern/dependency/DependentContainer.java
+++ kern-dependency/src/main/java/me/bottdev/kern/dependency/DependentContainer.java
@@ -1,12 +1,28 @@
 package me.bottdev.kern.dependency;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /// Container that stores a list of [DependencyAware] objects.
 /// Used by resolver as an argument.
 public interface DependentContainer<K, T extends DependencyAware<K>> {
 
-    /// Returns a list of [[DependencyAware]] objects.
-    List<T> dependents();
+    boolean isEmpty();
+
+    /// @return a list of [K] keys.
+    Set<K> keys();
+
+    /// @return a list of [DependencyAware] objects.
+    Collection<T> values();
+
+    /// @return Indicates whether the container has an object with a specified key.
+    boolean contains(K key);
+
+    /// @return Object associated with a provided key or null.
+    T get(K key);
+
+    /// @return Map with objects associated with their keys.
+    Map<K, T> toMap();
 
 }
--- kern-dependency/src/main/java/me/bottdev/kern/dependency/containers/SimpleDependentContainer.java
+++ kern-dependency/src/main/java/me/bottdev/kern/dependency/containers/SimpleDependentContainer.java
@@ -3,32 +3,30 @@ package me.bottdev.kern.dependency.containers;
 import me.bottdev.kern.dependency.DependencyAware;
 import me.bottdev.kern.dependency.DependentContainer;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /// Simple implementation of [DependentContainer]. Immutable.
 /// Use [SimpleDependentContainer#builder()] method to create a container with a builder.
 public record SimpleDependentContainer<K, T extends DependencyAware<K>>(
-        List<T> dependents
+        Map<K, T> dependentsMap
 ) implements DependentContainer<K, T> {
 
     public static class Builder<K, T extends DependencyAware<K>> {
 
-        private final List<T> dependents = new ArrayList<>();
+        private final Map<K, T> dependentsMap = new HashMap<>();
 
         public Builder<K, T> add(T dependent) {
-            dependents.add(dependent);
+            dependentsMap.put(dependent.dependencyKey(), dependent);
             return this;
         }
 
         public Builder<K, T> add(List<T> dependents) {
-            this.dependents.addAll(dependents);
+            dependents.forEach(this::add);
             return this;
         }
 
         public SimpleDependentContainer<K, T> build() {
-            return new SimpleDependentContainer<>(dependents);
+            return new SimpleDependentContainer<>(dependentsMap);
         }
 
     }
@@ -38,8 +36,32 @@ public record SimpleDependentContainer<K, T extends DependencyAware<K>>(
     }
 
     @Override
-    public List<T> dependents() {
-        return Collections.unmodifiableList(dependents);
+    public boolean isEmpty() {
+        return dependentsMap.isEmpty();
     }
 
+    @Override
+    public Set<K> keys() {
+        return Collections.unmodifiableSet(dependentsMap.keySet());
+    }
+
+    @Override
+    public Collection<T> values() {
+        return Collections.unmodifiableCollection(dependentsMap.values());
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return dependentsMap.containsKey(key);
+    }
+
+    @Override
+    public T get(K key) {
+        return dependentsMap.get(key);
+    }
+
+    @Override
+    public Map<K, T> toMap() {
+        return Collections.unmodifiableMap(dependentsMap);
+    }
 }
