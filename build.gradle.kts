140d7a1a887ac4492a991371cf2e32b79047d769
_xBott <roma.plakhotniuk07@gmail.com>
2026-08-15 14:06:30 +0200

    chore: updated project version to 0.0.38-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val rootEnvVars = DotEnvBuilder.dotEnv {
 
 allprojects {
     project.group = "me.bottdev"
-    project.version = "0.0.37-SNAPSHOT"
+    project.version = "0.0.38-SNAPSHOT"
 
     repositories {
         mavenCentral()
