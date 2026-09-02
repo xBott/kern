fdaf5c13fea129f30f38801dd4877f3d6b79b017
_xBott <roma.plakhotniuk07@gmail.com>
2026-09-02 17:37:01 +0200

    chore: added new modules

---
+++ kern-memory/build.gradle.kts
@@ -0,0 +1,13 @@
+dependencies {
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit.jupiter)
+
+    testImplementation(libs.assertj.core)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+}
